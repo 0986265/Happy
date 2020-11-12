9694ca05467f8fa7beeb01332a2c38f0d8164fc8
JesseStolk <jesse1606@gmail.com>
2020-11-12 14:49:31 +0100

    Added buttons and a screen script for login and get users

---
+++ App/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ScreenManager : MonoBehaviour
+{
+    public GameObject currentScreen;
+    public GameObject startScreen;
+
+    void Start()
+    {
+        currentScreen = startScreen;
+        currentScreen.SetActive(true);
+    }
+
+    public void OpenScreen(GameObject screen)
+    {
+        currentScreen.SetActive(false);
+        currentScreen = screen;
+        currentScreen.SetActive(true);
+    }
+
+    public void GetUsersButton()
+    {
+        StartCoroutine(GetUsers());
+    }
+    public void LoginUser()
+    {
+        StartCoroutine(Login("test@student.com", "testing1234"));
+    }
+
+    IEnumerator GetUsers()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://boostworks.online/GetStudents.php"))
+        {
+            yield return www.Send();
+
+            if(www.isNetworkError || www.isHttpError)
+            {
+                Debug.LogError(www.error);
+            } else
+            {
+                Debug.Log(www.downloadHandler.text);
+
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+    IEnumerator Login(string email, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginEmail", email);
+        form.AddField("loginPass", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://boostworks.online/Login.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+}
