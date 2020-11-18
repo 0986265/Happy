ad854d59e816dbbe05eea447511a2ed9aa606be2
Sonny <sonnylo@hotmail.com>
2020-11-18 13:17:54 +0100

    Streamlined database connection code
    Made a separate db connection class for connection.

--- Php/DatabaseConn.php
+++ Php/DatabaseConn.php
@@ -1,25 +1,36 @@
 <?php
 
-class DbConn{
+    class DbConn{
 
-    private $servername = "boostworks.online";
-    private $username = "boostworksonline_ced-app";
-    private $password = "SyoYiTgKYj$6925&aF3y";
-    private $db = "boostworksonline_ced";
+        private $servername = "boostworks.online";
+        private $username = "boostworksonline_ced-app";
+        private $password = "SyoYiTgKYj$6925&aF3y";
+        private $db = "boostworksonline_ced";
+        private $conn;
 
-    function __construct(){
-        
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $db);
-
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+        function __construct(){
+            try{
+                // Create connection
+                $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->db);
+            } catch(Exception $error){
+                echo "<h1>Connection error {$error->message()}" ;
+            }
         }
+
+        public function DbSelect($selected_columns, $table, $where_claus = null){
+            $sql = "SELECT {$selected_columns} FROM {$table} ". ($where_claus != null ? "WHERE {$where_claus};" : ";");
+            return $this->conn->query($sql);
+
+        }
+
+        public function DbUpdate($table, $update, $where_claus = null){
+            // $sql = "Update {$table} SET {$update} " . ($where_claus != null ? "WHERE {$where_claus};" : ";");
+            // return $this->conn->query($sql);
+
+            $sql = "UPDATE {$table} SET {$update} ". ($where_claus != null ? "WHERE {$where_claus};" : ";");
+            return $this->conn->query($sql);
+        }
+
     }
-
-}
-
-
 
 ?>
--- Php/Reset.php
+++ Php/Reset.php
@@ -1,46 +1,39 @@
 <?php
 
-$servername = "boostworks.online";
-$username = "boostworksonline_ced-app";
-$password = "SyoYiTgKYj$6925&aF3y";
-$db = "boostworksonline_ced";
+    require 'DatabaseConn.php';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db);
+    // Create connection
+    $conn = new DbConn;
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+    // Get data
+    // $sql = "SELECT * FROM students WHERE email = 'test@student.com'";
+    $result = $conn->DbSelect('*', 'students', 'email = "test@student.com"');
 
-// Get data
-$sql = "SELECT * FROM students WHERE email = 'test@student.com'";
-$result = $conn->query($sql);
+    if($result->num_rows > 0){
 
-if($result->num_rows > 0){
-
-    while($row = $result->fetch_assoc()){
-        /*
-        * Set max lenght of the new password
-        * Shuffle the string and return a new string with a length of 10
-        * Hashed the password with php function
-        */ 
-        $length = 10;    
-        $newPass = substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'),0,$length);
-        $hashedPass = password_hash($newPass, PASSWORD_DEFAULT, ['cost' => 12]);
-     
-        $newSQL = "UPDATE students SET nickname='{$newPass}', password='{$hashedPass}' WHERE id={$row['id']}";
-
-        if($conn->query($newSQL)){
-            echo "New passport is {$newPass}";
-        }else{
-            echo "Password failed";
-        }
+        while($row = $result->fetch_assoc()){
+            /*
+            * Set max lenght of the new password
+            * Shuffle the string and return a new string with a length of 10
+            * Hashed the password with php function
+            */ 
+            $length = 10;    
+            $newPass = substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'),0,$length);
+            $hashedPass = password_hash($newPass, PASSWORD_DEFAULT, ['cost' => 12]);
         
+            $update = "nickname='{$newPass}', password='{$hashedPass}' ";
+            $where_claus = "id={$row['id']}";
+
+            if($conn->DbUpdate('students', $update, $where_claus)){
+                echo "New passport is {$newPass}";
+            }else{
+                echo "Password failed";
+            }
+            
+        }
+    }else {
+        echo "Email does not exist";
     }
-}else {
-    echo "Email does not exist";
-}
 
 
 ?>
