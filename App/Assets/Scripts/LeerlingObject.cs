9b9b6fff513531a38250e03d80c9cedb293c4f26
JesseStolk <jesse1606@gmail.com>
2020-11-18 10:33:42 +0100

    Added new scripts and more

---
+++ App/Assets/Scripts/LeerlingObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeerlingObject
+{
+    public int id;
+
+    public string firstname;
+    public string lastname;
+
+    public string email;
+    public string nickname;
+
+    //public string color;
+    //public string interests;
+
+    ///public string avatar;
+
+    public static LeerlingObject CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<LeerlingObject>(jsonString);
+    }
+}
