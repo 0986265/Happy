cfcdd95a3643105b09360d9297c4f267fde8ac71
Sonny <sonnylo@hotmail.com>
2020-11-19 10:30:21 +0100

    Refactor Db conn php

--- Php/DatabaseConn.php
+++ Php/DatabaseConn.php
@@ -8,6 +8,7 @@
         private $db = "boostworksonline_ced";
         private $conn;
 
+        // Connect to database once this class is instantiated
         function __construct(){
             try{
                 // Create connection
@@ -17,17 +18,22 @@
             }
         }
 
+        // Select query with an optional last parameter
         public function DbSelect($selected_columns, $table, $where_claus = null){
             $sql = "SELECT {$selected_columns} FROM {$table} ". ($where_claus != null ? "WHERE {$where_claus};" : ";");
             return $this->conn->query($sql);
 
         }
 
+        // Update query with an optional last parameter
         public function DbUpdate($table, $update, $where_claus = null){
-            // $sql = "Update {$table} SET {$update} " . ($where_claus != null ? "WHERE {$where_claus};" : ";");
-            // return $this->conn->query($sql);
-
             $sql = "UPDATE {$table} SET {$update} ". ($where_claus != null ? "WHERE {$where_claus};" : ";");
+            return $this->conn->query($sql);
+        }
+
+        // Insert query 
+        public function DbInsert($table, $columns, $values){
+            $sql = "INSERT INTO {$table} ({$columns}) VALUES ({$values})";
             return $this->conn->query($sql);
         }
 
