9b99b8b8e1d2720314803a8ca2caf6e4cfd772b0
Boost9898 <bls.place@gmail.com>
2020-11-19 13:39:35 +0100

    Fixed send feeling

--- Php/SendFeeling.php
+++ Php/SendFeeling.php
@@ -5,17 +5,15 @@ require 'DatabaseConn.php';
 $conn = new DbConn;
 
 // Variables from user app (Unity)
-$studentId = $_POST['userID']; // Fill with unique student ID
+$studentId = $_POST['studentId']; // Fill with unique student ID
 $feelingScore = $_POST['feelingScore']; // Fill with feeling (1 to 5)
 $feelingComment = htmlentities($_POST['feelingComment']); // Fill with optional comment
 $timestamp = date('Y-m-d'); // Current day (year, month, day)
 
-$sql = "INSERT INTO feelings (student_id, score, comment, created_at) VALUES ('$studentId', '$feelingScore', '$feelingComment', '$timestamp')";
-
-$feedback = $conn->DbInsert('feelings', 'student_id, score, comment, created_at', "'{$studentId}', '{$feelingScore}', '{$feelinComment}', '{$timestamp}'");
+$feedback = $conn->DbInsert('feelings', 'student_id, score, comment, created_at', "{$studentId}, {$feelingScore}, '{$feelingComment}', '{$timestamp}'");
 
 if ($feedback == false) {
-  console.log("It broke, feedback boolean is false");
+  echo("It broke, feedback boolean is false");
 }
 
 $conn->DbClose();
