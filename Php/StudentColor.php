de638dd65ce90fac37d7d1bae32017d6201dbfdd
Boost9898 <bls.place@gmail.com>
2020-11-24 12:13:54 +0100

    Students can update their profile with a fixed color (red, green, blue, yellow, purple)

---
+++ Php/StudentColor.php
@@ -0,0 +1,23 @@
+<?php
+require 'DatabaseConn.php';
+
+// Create connection
+$conn = new DbConn;
+
+// Variables from user app (Unity)
+$studentId = $_POST['studentId']; // Fill with unique student ID
+$selectedColor = $_POST['selectedColor']; // Fill with color (red, geen, blue, yellow, purple,)
+
+// Update data
+$update = "color='{$selectedColor}'";
+$where_claus = "id={$studentId}";
+
+if($conn->DbUpdate('students', $update, $where_claus)){
+  echo "Color updated";
+}else{
+  echo "Color update failed";
+}
+
+$conn->DbClose();
+
+?>
