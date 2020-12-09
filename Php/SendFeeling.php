766cb73776ab38aa62ec610ddace5e58f8ef0d28
Boost9898 <bls.place@gmail.com>
2020-12-09 12:54:08 +0100

    no message

--- Php/SendFeeling.php
+++ Php/SendFeeling.php
@@ -7,6 +7,9 @@ $conn = new DbConn;
 // Variables from user app (Unity)
 $studentId = $_POST['studentId']; // Fill with unique student ID
 $feelingScore = $_POST['feelingScore']; // Fill with feeling (1 to 5)
+// !!!--- mysqli_real_escape_string ---!!!
+// !!!--- mysqli_real_escape_string ---!!!
+// !!!--- mysqli_real_escape_string ---!!!
 $feelingComment = $_POST['feelingComment']; // Fill with optional comment
 $timestamp = date('Y-m-d'); // Current day (year, month, day)
 
