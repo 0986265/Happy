41198e5789411145ac1757aa3d7d2e2690b1ab18
Jesse Stolk <jesse1606@gmail.com>
2020-12-09 12:53:47 +0100

    Changed HTML entities to mysqli_escape_string

--- Php/SendFeeling.php
+++ Php/SendFeeling.php
@@ -7,7 +7,7 @@ $conn = new DbConn;
 // Variables from user app (Unity)
 $studentId = $_POST['studentId']; // Fill with unique student ID
 $feelingScore = $_POST['feelingScore']; // Fill with feeling (1 to 5)
-$feelingComment = htmlentities($_POST['feelingComment']); // Fill with optional comment
+$feelingComment = mysqli_real_escape_string($_POST['feelingComment']); // Fill with optional comment
 $timestamp = date('Y-m-d'); // Current day (year, month, day)
 
 $feedback = $conn->DbInsert('feelings', 'student_id, score, comment, created_at', "{$studentId}, {$feelingScore}, '{$feelingComment}', '{$timestamp}'");
